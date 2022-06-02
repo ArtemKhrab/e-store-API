ab5e594914ad5314cc00bfc9081365a0aa2a427e
ArtemKhrab <a.chastener@gmail.com>
2022-06-02 13:51:30 +0200

    psycopg2 added + environ connector for db

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import os
 
 from flask import Flask
 from flask_restful import Api 
@@ -15,7 +16,7 @@ app = Flask(__name__)
 app.secret_key = "asfA13nQJSdp12nASdjqsdl39rasjSd2"
 api = Api(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
